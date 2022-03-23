21e9c274875ec525ab9060067fd2b0e80e98f6af
namnd <namnguyennd7428@gmail.com>
2022-03-23 23:22:53 +0700

    template engines

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.get('/user', (req, res) => {
-    res.send('User list')
+    res.render("users/index", {
+        users: [
+            { id: 1, name: "Nam" },
+            { id: 2, name: "Kelvin" },
+        ]
+    });
 })
+
 
 app.listen(port, function () {
     console.log("Server listening on port " + port);
