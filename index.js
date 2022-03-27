5bb394b569063d7b09e875099df38af9a16252a2
namnd <namnguyennd7428@gmail.com>
2022-03-27 23:11:29 +0700

    query parameters

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const port = 3000;
 
+let users = [
+    { id: 1, name: "Nam" },
+    { id: 2, name: "Kelvin" },
+    { id: 3, name: "Kelvin Kien" },
+]
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -12,14 +18,20 @@ app.get('/', (req, res) => {
 
 app.get('/user', (req, res) => {
     res.render("users/index", {
-        users: [
-            { id: 1, name: "Nam" },
-            { id: 2, name: "Kelvin" },
-            { id: 3, name: "Kelvin Kien" },
-        ]
+        users
     });
 })
 
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let matchedUser = users.filter(item => item.name.indexOf(q) !== -1);
+    // console.log(matchedUser);
+
+    res.render('users/index', {
+        users: matchedUser
+    })
+    // console.log(req.query);
+})
 
 app.listen(port, function () {
     console.log("Server listening on port " + port);
