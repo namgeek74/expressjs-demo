54026e83c6276c044a3bcea83759e8000ee52aab
namnd <namnguyennd7428@gmail.com>
2022-03-31 22:47:13 +0700

    middleware and res locals variable

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -34,23 +34,6 @@ module.exports.detail = (req, res) => {
 };
 
 module.exports.postCreate = (req, res) => {
-  let errors = [];
-  if (!req.body.name) {
-    errors.push('Name is required.');
-  }
-  if (!req.body.phone) {
-    errors.push('Phone is required.');
-  }
-
-  if (errors.length) {
-    res.render('users/create', {
-      errors: errors,
-      values: req.body,
-    });
-
-    return;
-  }
-
   req.body.id = shortid.generate();
   db.get('users').push(req.body).write();
   res.redirect('/users');
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const { route } = require('express/lib/application');
 const controllers = require('../controllers/users.controller');
+const validate = require("../validate/users.validate");
 
 const router = express.Router();
 
 router.get('/', controllers.index);
+
+function middleware1(req, res, next) {
+    res.locals.success = true;
+    next();
+}
+
+function middleware2(req, res, next) {
+    console.log(res.locals.success);
+    res.send("Hello middleware")
+}
+
+router.get("/test-middleware", middleware1, middleware2);
+
 router.get('/search', controllers.search);
 
 router.get('/create', controllers.create);
 
 router.get('/:userId', controllers.detail);
 
-router.post('/create', controllers.postCreate);
+router.post('/create', validate.validatePostCreate, controllers.postCreate);
 
 module.exports = router;
